17d759eef321e1e12fc486a3cbaee823b5acbb15
drozencarl23 <rozen.delacruz@ph.softvision.global>
2022-06-21 20:58:35 +0800

    Feature Create Account Form

--- src/app/create-account/create-account.component.ts
+++ src/app/create-account/create-account.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountTypeEnum } from '../enums/AccountTypeEnum';
+import { AccountTypeUtility } from '../enums/AccountTypeUtility';
 
 @Component({
   selector: 'app-create-account',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-account.component.css']
 })
 export class CreateAccountComponent implements OnInit {
-
-  constructor() { }
+  accountTypeList: AccountTypeEnum[] = AccountTypeUtility.getListAccounts();
+  createAccountForm: FormGroup;
 
   ngOnInit(): void {
+    this.createAccountForm = new FormGroup({
+      'accountName': new FormControl('', [Validators.required]),
+      'accountNumber': new FormControl('', [Validators.required]),
+      'accountType': new FormControl(AccountTypeEnum.CHECKINGS, [Validators.required]),
+      'balance': new FormControl('', [Validators.required])
+    });
   }
 
+  onSubmit(): void {
+    this.createAccountForm.markAllAsTouched();
+  }
 }
---
+++ src/app/enums/AccountTypeEnum.ts
@@ -0,0 +1,4 @@
+export enum AccountTypeEnum {
+    SAVINGS = 'SA',
+    CHECKINGS = 'CA'
+}
